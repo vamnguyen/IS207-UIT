a8785272ca3c12d196b37aa2c5d7d37a72625c2d
Minh Nguyen <anhminh.dev@gmail.com>
2025-10-15 11:14:26 +0700

    feat: Add 'https://rerent-admin.vercel.app' to allowed origins in CORS configuration

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'https://rerent-three.vercel.app', 'https://rerent-shop.vercel.app'], // Next.js URL
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'https://rerent-three.vercel.app', 'https://rerent-shop.vercel.app', 'https://rerent-admin.vercel.app'], // Next.js URL
 
     'allowed_origins_patterns' => [],
 
